ec6c555101af82b5c32b972f44e0cb592f8021c3
dalfaia <damien_alfaia@live.fr>
2016-05-27 22:45:24 +0200

    suite commit 27/05/16

---
+++ PronosContest/Code/PronosContestControllerBase.cs
@@ -0,0 +1,58 @@
+﻿using PronosContest.DAL.Authentification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PronosContest.Code
+{
+	public abstract class PronosContestControllerBase : Controller
+	{
+		protected int? UserID
+		{
+			get
+			{
+				if (Session["PronosContest.UserID"] != null && Session["PronosContest.UserID"] is int)
+					return (int)Session["PronosContest.UserID"];
+				return null;
+			}
+			set
+			{
+				Session["PronosContest.UserID"] = value;
+			}
+		}
+
+		protected CompteUtilisateur CurrentUser
+		{
+			get
+			{
+				if (Session["PronosContest.CurrentUser"] != null && Session["PronosContest.CurrentUser"] is CompteUtilisateur)
+					return (CompteUtilisateur)Session["PronosContest.CurrentUser"];
+				return null;
+			}
+			set
+			{
+				Session["PronosContest.CurrentUser"] = value;
+			}
+		}
+
+		public PronosContestControllerBase()
+		{
+		}
+
+		protected override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			base.OnActionExecuting(filterContext);
+		}
+		public ActionResult RedirectToLocal(string returnUrl)
+		{
+			if (Url.IsLocalUrl(returnUrl))
+			{
+				return Redirect(returnUrl);
+			}
+			return RedirectToAction("Index", "Public");
+		}
+	}
+}
---
+++ PronosContest/Models/PronosticsModels.cs
@@ -0,0 +1,21 @@
+﻿using PronosContest.DAL.Pronos;
+using PronosContest.DAL.Shared;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace PronosContest.Models
+{
+	public class SearchConcoursModel
+	{
+		[DataType(DataType.Text)]
+		public string Recherche { get; set; }
+
+		public List<Concours> Resultats { get; set; }
+
+		public SearchConcoursModel()
+		{
+			this.Resultats = new List<Concours>();
+		}
+	}	
+}
